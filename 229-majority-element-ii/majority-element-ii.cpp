22d5dac68c4f95505fce72bcc57aefdfd2c2baac
Achala Karn <achalakarn15@gmail.com>
2026-03-28 10:06:23 +0530

    Time: 0 ms (100.00%) | Memory: 23.6 MB (64.35%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -16,19 +16,68 @@ public:
         // v.push_back(candidate);
         // return v;
 
-        map<int,int>freqArray;
-        vector<int>v;
-        for(auto num : nums)
+        // map<int,int>freqArray;
+        // vector<int>v;
+        // for(auto num : nums)
+        // {
+        //     freqArray[num]++;
+        // }
+        // for(auto num : freqArray)
+        // {
+        //     if(num.second > (nums.size()/3))
+        //     {
+        //         v.push_back(num.first);
+        //     }
+        // }
+        // return v;
+        int count1=0,count2=0,candidate1=0,candidate2=0;
+        for(int num : nums)
         {
-            freqArray[num]++;
-        }
-        for(auto num : freqArray)
-        {
-            if(num.second > (nums.size()/3))
+            if(num == candidate1)
             {
-                v.push_back(num.first);
+                count1++;
+            }
+            else if(num==candidate2)
+            {
+                count2++;
+            }
+            else if(count1==0)
+            {
+                candidate1=num;
+                count1=1;
+            }
+            else if(count2==0)
+            {
+                candidate2=num;
+                count2=1;
+            }
+            else
+            {
+                count1--;
+                count2--;
             }
         }
-        return v;
+        vector<int> result;
+        count1=count2=0;
+        for(int num:nums)
+        {
+            if(num==candidate1)
+            {
+                count1++;
+            }
+            else if(num==candidate2)
+            {
+                count2++;
+            }
+        }
+        if(count1>nums.size() / 3)
+        {
+            result.push_back(candidate1);
+        }
+        if(count2>nums.size()/3)
+        {
+            result.push_back(candidate2);
+        }
+        return result;
     }
 };
