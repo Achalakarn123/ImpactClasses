5979a8dcbfb55bf9ebe4e1bbff4692d8abd5633b
Achala Karn <achalakarn15@gmail.com>
2026-04-06 01:49:05 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (75.53%) - LeetSync

--- 810-valid-tic-tac-toe-state/valid-tic-tac-toe-state.cpp
+++ 810-valid-tic-tac-toe-state/valid-tic-tac-toe-state.cpp
@@ -1,36 +1,39 @@
 class Solution {
 public:
-    bool validTicTacToe(vector<string>& board) 
-    {
-            int X=0, O=0;
-            for(auto row : board)
+    bool validTicTacToe(vector<string>& board) {
+        int X=0, O=0;
+        for(auto row : board)
+        {
+            for(auto c : row)
             {
-                for(auto c : row)
-                {
-                    if(c=='X') X++;
-                    else if (c=='O') O++;
-                }
+                if(c=='X') X++;
+                else if(c=='O') O++;
             }
-            if(!(X==O || X==O+1)) return false;
-
-            bool xWin=checkWin(board, 'X');
-            bool oWin=checkWin(board, 'O');
-
-            if(xWin && oWin) return false;
-            if(xWin && X!= O + 1) return false;
-            if(oWin && X!=O) return false;
-
-            return true;
         }
+        if(!(X==O || X==O+1)) return false;
+
+        bool xWin=checkWin(board,'X');
+        bool oWin=checkWin(board,'O');
+
+        if(xWin && oWin)return false;
+        if(xWin && X!=O+1) return false;
+        if(oWin && X!=O) return false;
+        return true;
+    }
     private:
-          bool checkWin(vector<string>&b, char p)
-          {
+           bool checkWin(vector<string>&b,char p)
+           {
             for(int i=0;i<3;i++)
             {
-                if(b[i][0] == p && b[i][1] == p && b[i][2]==p) return true;
-                if(b[0][i] == p && b[1][i] == p && b[2][i]==p) return true;
+                if(b[i][0]==p && b[i][1]==p && b[i][2]==p)
+                {
+                    return true;
+                }
+                if(b[0][i]==p && b[1][i]==p && b[2][i]==p)
+                {
+                    return true;
+                }
             }
-            return (b[0][0] == p && b[1][1] == p && b[2][2]==p) ||
-            (b[0][2] == p && b[1][1]==p && b[2][0]==p);
-          }
+            return (b[0][0]==p && b[1][1]==p && b[2][2]==p) || (b[0][2]==p && b[1][1]==p && b[2][0]==p);
+           }
 };
